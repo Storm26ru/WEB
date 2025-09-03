c8615179fecb35c1616975058905514def187916
Storm <dpkstav@gmail.com>
2025-09-03 22:21:48 +0300

    file classwork done

--- JavaScript/02-Forms/functions.js
+++ JavaScript/02-Forms/functions.js
@@ -1,29 +1,38 @@
 // JavaScript source code
-function Power()
-{
+function Power() {
     let base = document.getElementById("base").value;
     let exponent = document.getElementById("exponent").value;
     document.getElementById("power").innerHTML = base ** exponent;
-
 }
 document.addEventListener("mousemove", GetMouseCoords);
-function GetMouseCoords(event)
-{
+function GetMouseCoords(event) {
     let x = event.clientX;
     let y = event.clientY;
-    document.getElementById("mouse").innerHTML = `X=${x}, Y=${y}`;
+    document.getElementById("mouse").innerHTML = `X = ${x}, Y = ${y}`;
 }
-
-function SwitchBackground()
-{
+function SwitchBackground() {
     let switchBackground = document.getElementById("switch-background");
-    if (switchBackground.style.backgroundImage === 'url("img/moon.png")')
-    {
-        switchBackground.style.backgroundImage = 'url("img/sun.png")';
-    }
-    else
-    {
-        switchBackground.style.backgroundImage = 'url("img/moon.png")';
-    }
+    //if(switchBackground.style.backgroundImage === 'url("img/moon.png")')
+    //{
+    //    switchBackground.style.backgroundImage = 'url("img/sun.png")';
+    //    document.body.className = 'dark';
+    //}
+    //else
+    //{
+    //    switchBackground.style.backgroundImage = 'url("img/moon.png")';
+    //    document.body.className = 'light';
+    //}
+    let delay = document.getElementById('delay').value;
+    document.body.style.transition = `background-color ${delay}s, color ${delay}s`;
+    document.getElementById('switch-background').style.transition = `background-image ${delay}s`;
+    document.body.className = document.body.className === "light" ? "dark" : "light";
+}
+function UploadPhoto() {
+   let image = document.getElementById('photo');
+   let students_photo = document.getElementById('students-photo');
+   let filename = students_photo.value.split('\\');
+    filename = filename[filename.length - 1];
+    image.src = filename;
+    
     
 }
