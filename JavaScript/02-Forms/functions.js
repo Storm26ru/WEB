9706300dc13e358f8e4ef38233f2b2502e610fae
Storm <dpkstav@gmail.com>
2025-09-14 10:05:05 +0300

    countdown timer done

--- JavaScript/02-Forms/functions.js
+++ JavaScript/02-Forms/functions.js
@@ -12,27 +12,110 @@ function GetMouseCoords(event) {
 }
 function SwitchBackground() {
     let switchBackground = document.getElementById("switch-background");
-    //if(switchBackground.style.backgroundImage === 'url("img/moon.png")')
-    //{
-    //    switchBackground.style.backgroundImage = 'url("img/sun.png")';
-    //    document.body.className = 'dark';
-    //}
-    //else
-    //{
-    //    switchBackground.style.backgroundImage = 'url("img/moon.png")';
-    //    document.body.className = 'light';
-    //}
     let delay = document.getElementById('delay').value;
     document.body.style.transition = `background-color ${delay}s, color ${delay}s`;
     document.getElementById('switch-background').style.transition = `background-image ${delay}s`;
     document.body.className = document.body.className === "light" ? "dark" : "light";
 }
 function UploadPhoto() {
-   let image = document.getElementById('photo');
-   let students_photo = document.getElementById('students-photo');
-   let filename = students_photo.value.split('\\');
+    let image = document.getElementById('photo');
+    let students_photo = document.getElementById('students-photo');
+    let filename = students_photo.value.split('\\');
     filename = filename[filename.length - 1];
     image.src = filename;
-    
-    
+    alert(filename);
 }
+function setImage() {
+    let filename = document.getElementById("students-photo");
+    let reader = new FileReader();
+    reader.onload = function (e) {
+        document.getElementById("photo").src = e.target.result;
+    }
+    reader.readAsDataURL(filename.files[0]);
+}
+///////////////////////////////////////////////////////////////////////////////////
+///////                             Timer                               ///////////
+
+document.body.onload = function tick_timer() {
+    let time = new Date();
+    document.getElementById("full-time").innerHTML = time;
+
+    //                  Current-time
+    document.getElementById("hours").innerHTML = addLeadingZero(time.getHours());
+    document.getElementById("minutes").innerHTML = addLeadingZero(time.getMinutes());
+    document.getElementById("seconds").innerHTML = addLeadingZero(time.getSeconds());
+
+    //                  Current-day
+    document.getElementById("year").innerHTML = addLeadingZero(time.getFullYear());
+    document.getElementById("month").innerHTML = addLeadingZero(time.getMonth() + 1);
+    document.getElementById("day").innerHTML = addLeadingZero(time.getDate());
+
+    //                  Weekday
+    document.getElementById("weekday").innerHTML = time.toLocaleDateString("ru", { weekday: 'long' });
+
+    //                  Checkboxes:
+    document.getElementById("current-date").style.visibility = document.getElementById("show-date").checked ? "visible" : "hidden";
+    document.getElementById("weekday").style.visibility = document.getElementById("show-weekday").checked ? "visible" : "hidden";
+
+    setTimeout(tick_timer, 100);
+}
+
+function addLeadingZero(number) {
+    return number < 10 ? "0" + number : number;
+}
+
+document.getElementById("btn-start").onclick = function startCountdownTimer() {
+    let targetDateControl = document.getElementById("target-date");
+    let targetTimeControl = document.getElementById("target-time");
+    let btnStart = document.getElementById("btn-start");
+    if (targetDateControl.value == "" || targetTimeControl.value == "") { alert("Select date and time"); return; }
+    targetDateControl.disabled = targetTimeControl.disabled = !targetDateControl.disabled;
+    if (btnStart.value === "Start") {
+        btnStart.value = "Stop";
+        document.getElementById("years").style.display = "none";
+        document.getElementById("months").style.display = "none";
+        document.getElementById("days").style.display = "none";
+        tickCountdown();
+    }
+    else {
+        btnStart.value = "Start";
+    }
+}
+
+function out(element, num)
+{
+    element.textContent = num;
+    if (!num) return; 
+    element.parentElement.style.display = 'block';
+    if (element.parentElement.id == "years") document.getElementById("months").style.display = document.getElementById("days").style.display = 'block';
+    if (element.parentElement.id == "months") document.getElementById("days").style.display = 'block';
+}
+function tickCountdown()
+{
+    if (!document.getElementById("target-time").disabled) return;
+    let now = new Date();
+    let targetDateControl = document.getElementById("target-date");
+    let targetTimeControl = document.getElementById("target-time");
+    let targetDate = targetDateControl.valueAsDate;
+    let targetTime = targetTimeControl.valueAsDate;
+    targetDate.setHours(targetTime.getUTCHours());
+    targetDate.setMinutes(targetTime.getUTCMinutes());
+    targetDate.setSeconds(targetTime.getUTCSeconds());
+    let difference = targetDate - now;
+    let res = new Date(difference);
+    if (res > 0) {
+        out(document.getElementById("years-unit"), res.getUTCFullYear() - 1970);
+        out(document.getElementById("months-unit"), res.getUTCMonth());
+        out(document.getElementById("days-unit"), res.getUTCDate() - 1);
+        document.getElementById("hours-unit").textContent = addLeadingZero(res.getUTCHours());
+        document.getElementById("minutes-unit").textContent = addLeadingZero(res.getUTCMinutes());
+        document.getElementById("seconds-unit").textContent = addLeadingZero(res.getUTCSeconds());
+        setTimeout(tickCountdown, 100);
+    }
+    else
+    {
+        alert("Time is over");
+        document.getElementById("btn-start").click();
+    }
+}
+
